38a54f93a4cf031dd82b99721748072e8c1cbdf4
LIM1113 <83012943+LIM1113@users.noreply.github.com>
2021-04-25 21:46:37 +0900

    practice1

---
+++ main.js
@@ -0,0 +1,34 @@
+let target = document.querySelector('#dynamic');
+
+function randomString(){
+  let stringArr = ["Learn to HTML","Learn to CSS","Learn to JaveScript","Learn to python","Learn to Ruby"];
+  let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+  let selectStringArr = selectString.split("");
+
+  return selectStringArr;
+}
+
+function resetTyping(){
+  target.textContent = "";
+  dynamic(randomString());
+}
+
+//한글자씩 텍스트 출력 함수
+function dynamic(randomArr){
+  if(randomArr.length > 0){
+    target.textContent += randomArr.shift();
+    setTimeout(function(){
+      dynamic(randomArr);
+    },80);
+  }else{
+    setTimeout(resetTyping,3000);
+  }
+}
+
+dynamic(randomString())
+
+function blink(){
+  target.classList.toggle("active");
+}
+
+setInterval(blink, 500);
